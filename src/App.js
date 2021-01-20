90ae1495fea302b02c39faed6fa5d5f572b54343
Kristian Hein <kristianhein@gmail.com>
2021-01-19 20:09:00 -0800

    removed test code

--- src/App.js
+++ src/App.js
@@ -71,7 +71,6 @@ const App = () => {
       // Extends WebViewer to allow loading media files (.mp3, .mp4, ogg, webm, etc.)
       const {
         loadAudio,
-        getFileURL,
       } = await initializeAudioViewer(
         instance,
         { license },
@@ -114,15 +113,6 @@ const App = () => {
             var xfdfString = await annotManager.exportAnnotations({ links: false, widgets: false, annotList: annotations });
             await saveXfdfString(DOCUMENT_ID, xfdfString);
             alert('Annotations saved successfully.');
-          }
-        });
-
-        header.push({
-          type: 'actionButton',
-          img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
-          title: 'Get file url',
-          onClick: async () => {
-            console.log(getFileURL());
           }
         });
       });
